5b90289552bc24bb94f14b46f959e9b7e051d6cd
InjiChoi <chldlswl1212@gmail.com>
2020-10-03 18:47:37 +0900

    2631 python 해설 추가

--- 탐색기반설계/2631.py
+++ 탐색기반설계/2631.py
@@ -1,24 +1,41 @@
 # 보물 찾기 -> 시간초과
-n, k = map(int, input().split())
-n_list = list(map(int, input().split()[:n]))
+# n, k = map(int, input().split())
+# n_list = list(map(int, input().split()[:n]))
 
-count = 0
-temp = 0
+# count = 0
+# temp = 0
 
-for i in range(0, n):
-    temp = n_list[i]
+# for i in range(0, n):
+#     temp = n_list[i]
 
-    if n_list[i] == k :
-        count += 1
-        continue
+#     if n_list[i] == k :
+#         count += 1
+#         continue
 
-    else :
-        for j in range(i+1, n):
-            temp += n_list[j]
-            if (temp > k):
-                break
-            elif (temp == k):
-                count += 1
-                break
+#     else :
+#         for j in range(i+1, n):
+#             temp += n_list[j]
+#             if (temp > k):
+#                 break
+#             elif (temp == k):
+#                 count += 1
+#                 break
 
-print(count)
+# print(count)
+
+n,k=map(int, input().split())
+a=[0]
+cnt=0
+a[1:n]=[int(i) for i in input().split()]
+
+for i in range(1, n+1) :
+    a[i]+=a[i-1]
+
+t=0
+for i in range(1, n+1) :
+    while a[i]-a[t]>k :
+        t+=1
+    if a[i]-a[t]==k :
+        cnt+=1
+
+print(cnt)
